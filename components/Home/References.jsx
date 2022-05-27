27b6b980eb946fd83baea1ddb0e6905d1b99090a
Ryan Le <ryanle@live.com>
2022-05-27 16:22:54 -0500

    update reference stacking:

--- components/Home/References.jsx
+++ components/Home/References.jsx
@@ -5,9 +5,11 @@ import Reference from './Reference';
 // Utilities
 import { fetchReferrals } from '../../data/fetch';
 import { useQuery } from 'react-query';
+import useMediaQuery from '../../hooks/useMediaQuery';
 
 const References = () => {
   const { data } = useQuery('referrals', fetchReferrals);
+  const isDesktopScreen = useMediaQuery(1200);
 
   return (
     <Box width='full'>
@@ -16,41 +18,74 @@ const References = () => {
       </Heading>
 
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={4}>
-        <Stack spacing={4}>
-          {data?.slice(0, 2).map((ref) => (
-            <Reference
-              key={ref.name}
-              name={ref.name}
-              title={ref.title}
-              imgPath={ref.imageUrl}
-              referral={ref.referral}
-            />
-          ))}
-        </Stack>
+        {isDesktopScreen && (
 
-        <Stack spacing={4}>
-          {data?.slice(2, 3).map((ref) => (
-            <Reference
-              key={ref.name}
-              name={ref.name}
-              title={ref.title}
-              imgPath={ref.imageUrl}
-              referral={ref.referral}
-            />
-          ))}
-        </Stack>
 
-        <Stack spacing={4}>
-          {data?.slice(3, 10).map((ref) => (
-            <Reference
-              key={ref.name}
-              name={ref.name}
-              title={ref.title}
-              imgPath={ref.imageUrl}
-              referral={ref.referral}
-            />
-          ))}
-        </Stack>
+          <>
+            <Stack spacing={4}>
+              {data?.slice(0, 2).map((ref) => (
+                <Reference
+                  key={ref.name}
+                  name={ref.name}
+                  title={ref.title}
+                  imgPath={ref.imageUrl}
+                  referral={ref.referral}
+                />
+              ))}
+            </Stack>
+
+            <Stack spacing={4}>
+              {data?.slice(2, 4).map((ref) => (
+                <Reference
+                  key={ref.name}
+                  name={ref.name}
+                  title={ref.title}
+                  imgPath={ref.imageUrl}
+                  referral={ref.referral}
+                />
+              ))}
+            </Stack>
+
+            <Stack spacing={4}>
+              {data?.slice(4, 10).map((ref) => (
+                <Reference
+                  key={ref.name}
+                  name={ref.name}
+                  title={ref.title}
+                  imgPath={ref.imageUrl}
+                  referral={ref.referral}
+                />
+              ))}
+            </Stack>
+          </>
+        )}
+
+        {!isDesktopScreen && (
+          <>
+            <Stack spacing={4}>
+              {data?.slice(0, 3).map((ref) => (
+                <Reference
+                  key={ref.name}
+                  name={ref.name}
+                  title={ref.title}
+                  imgPath={ref.imageUrl}
+                  referral={ref.referral}
+                />
+              ))}
+            </Stack>
+            <Stack spacing={4}>
+              {data?.slice(3, 10).map((ref) => (
+                <Reference
+                  key={ref.name}
+                  name={ref.name}
+                  title={ref.title}
+                  imgPath={ref.imageUrl}
+                  referral={ref.referral}
+                />
+              ))}
+            </Stack>
+          </>
+        )}
       </SimpleGrid>
     </Box>
   );
